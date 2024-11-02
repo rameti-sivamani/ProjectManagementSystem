4cae010ef445985e37290631c6fde2708c467bde
rameti sivamani <sivamanirameti494@gmail.com>
2024-11-02 12:45:53 +0530

    Commit all changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("This is a Test case");
+const express=require("express");
+const path=require("path");
+const app = express();
+
+app.set('views','views');
+app.set('view engine','ejs');
+app.use("/",(req,res,next)=>{
+    app.render('index.ejs');
+});
+app.listen('3000',()=>{
+    console.log("Server Running on Port 3000");
+})
