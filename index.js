259d235c0970b7d3467c77092f6e113267c44759
rameti sivamani <sivamanirameti494@gmail.com>
2024-11-03 00:15:32 +0530

    Commit the all changes

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express=require("express");
-const path=require("path");
+const path=require('path');
+const bodyParser=require('body-parser');
 const app = express();
 
-app.set('views','views');
 app.set('view engine','ejs');
-app.use("/",(req,res,next)=>{
-    app.render('index.ejs');
+app.set('views','views');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+// app.use("/",(req,res,next)=>{
+//     console.log("Home");
+//     res.render('index.ejs');
+// });
+app.use("/register",(req,res,next)=>{
+    console.log("Home");
+    res.render('registration.ejs');
 });
 app.listen('3000',()=>{
     console.log("Server Running on Port 3000");
